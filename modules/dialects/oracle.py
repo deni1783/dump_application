02d5bb19a1f8bdbdff0d6cf2f721017acd20db22
denis <deni1783@gmail.com>
2017-05-27 13:47:07 +0300

    add wrap for all dialects

---
+++ modules/dialects/oracle.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets
+
+
+class Settings(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.initUI()
+
+    def initUI(self):
+
+        vbox = QtWidgets.QVBoxLayout()
+
+
+        wrap_vbox = QtWidgets.QVBoxLayout()
+        lbl = QtWidgets.QLabel('test lbl for Oracle')
+        wrap_vbox.addWidget(lbl)
+
+
+
+
+        # Возвращаем в основной макет
+        self.out_window = wrap_vbox
+
+
+
